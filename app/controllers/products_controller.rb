7aeffe2c7f919af181e0bb9beb895f2c0fae7b13
Kryss <kryss.col@gmail.com>
2010-11-29 15:53:59 +0100

    add logo

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+end
